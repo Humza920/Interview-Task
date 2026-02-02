ec7acf08fb8f961837a62e38df33e6a9f973bdcf
Humza920 <humza.rehman.779@gmail.com>
2026-02-02 16:30:25 +0500

    PUHSHING

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,11 @@ const authRouter = require("./routes/authRoutes")
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: '*' }))
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
+
 app.use(cookieParser())
 app.use("/api/auth", authRouter)
 
